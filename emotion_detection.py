2314662d34f413b355545b0bd60dce07877510e8
ahmedabbasselim <a.selim1979@gmail.com>
2025-08-12 08:31:11 -0400

    formatted_response_a

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,23 @@
 import requests
-
+import json
 def emotion_detector(text_to_analyze):
+    # URL of the EMOTION detection service
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    # header to specify the model ID for the emotion detection service
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    json = { "raw_document": { "text": text_to_analyze } }
-    response = requests.post(url, json = json, headers = headers)
-    return response.text
+    # Request payload in the expected format
+    myobj = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url, json = myobj, headers = headers)
+    
+    # Parsing the JSON response from the API (Convert text to Python Dictionary)
+    formatted_response = json.loads(response.text) 
+    
+    anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+
+    emotion_dict = formatted_response['emotionPredictions'][0]['emotion']
+
+    return emotion_dict
