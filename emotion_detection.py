c6fdc5d5fcd71c8ab094dc38d80c437009aab4f6
ahmedabbasselim <a.selim1979@gmail.com>
2025-08-13 05:31:53 -0400

    emotions plus dominant emotion

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,5 +1,6 @@
 import requests
 import json
+
 def emotion_detector(text_to_analyze):
     # URL of the EMOTION detection service
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -9,15 +10,13 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers = headers)
     
-    # Parsing the JSON response from the API (Convert text to Python Dictionary)
+    # Parsing the JSON response from the API
     formatted_response = json.loads(response.text) 
-    
-    anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
-    disgust = formatted_response['emotionPredictions'][0]['emotion']['disgust']
-    fear = formatted_response['emotionPredictions'][0]['emotion']['fear']
-    joy = formatted_response['emotionPredictions'][0]['emotion']['joy']
-    sadness = formatted_response['emotionPredictions'][0]['emotion']['sadness']
-
-    emotion_dict = formatted_response['emotionPredictions'][0]['emotion']
-
-    return emotion_dict
+    # Extracting emotion dictionary from the formatted response
+    emotion = formatted_response['emotionPredictions'][0]['emotion']
+    # Extracting dominant emotion from emotion dictionary
+    dominant_emotion_key = max(emotion, key = emotion.get)
+    #Adding new key to emotion dictionary
+    emotion['dominant_emotion'] = dominant_emotion_key
+    # Returning emotion dictionary after adding dominant emotion to it
+    return emotion
