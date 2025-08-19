d8f311341a70a6678527d197039599a6824cc838
ahmedabbasselim <a.selim1979@gmail.com>
2025-08-19 16:08:16 -0400

    Incorporate Error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -11,12 +11,17 @@ def emotion_detector(text_to_analyze):
     response = requests.post(url, json = myobj, headers = headers)
     
     # Parsing the JSON response from the API
-    formatted_response = json.loads(response.text) 
-    # Extracting emotion dictionary from the formatted response
-    emotion = formatted_response['emotionPredictions'][0]['emotion']
-    # Extracting dominant emotion from emotion dictionary
-    dominant_emotion_key = max(emotion, key = emotion.get)
-    #Adding new key to emotion dictionary
-    emotion['dominant_emotion'] = dominant_emotion_key
-    # Returning emotion dictionary after adding dominant emotion to it
-    return emotion
+    formatted_response = json.loads(response.text)
+
+    if response.status_code == 200: 
+        # Extracting emotion dictionary from the formatted response
+        emotion = formatted_response['emotionPredictions'][0]['emotion']
+        # Extracting dominant emotion from emotion dictionary
+        dominant_emotion_key = max(emotion, key = emotion.get)
+        #Adding new dominant_emotion_key to emotion dictionary
+        emotion['dominant_emotion'] = dominant_emotion_key
+        return emotion
+    elif response.status_code == 400:
+        emotion = {'anger': 'None', 'disgust': 'None', 'fear': 'None', 'joy': 'None', 'sadness': 'None', 'dominant_emotion': 'None'}
+        return emotion
+    
