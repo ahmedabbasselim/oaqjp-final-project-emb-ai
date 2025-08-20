5343f76f2cdba40bbba07e499ac2d8a8209c320b
ahmedabbasselim <a.selim1979@gmail.com>
2025-08-20 08:42:20 -0400

    static code analysis

--- server.py
+++ server.py
@@ -3,30 +3,23 @@ from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 app = Flask("Emotion Detection")
 @app.route("/emotionDetector")
-
 def emot_detector():
     """
-    Retrieve the text to analyze from the request arguments,
+    Retrieve the text to analyze from the request arguments, 
     then pass the text to the emotion_detector function
     """
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
-    # Extract the emotion value from the response
-    anger = response ['anger']
-    disgust = response ['disgust']
-    fear = response ['fear']
-    joy = response ['joy']
-    sadness = response ['sadness']
-    dominant_emotion = response ['dominant_emotion']
+    dominant_emotion = response['dominant_emotion']
     if dominant_emotion == 'None':
         return  "Invalid text! Please try again!."
     # Return a formatted string with the emotion
-    return (
-            "For the given statement, the system response is "
-            f"'anger':{anger}, 'disgust':{disgust}, 'fear':{fear}, 'joy':{joy} "
-            f"and 'sadness':{sadness}. "
-            f"The dominant emotion is {dominant_emotion}."
-    )
+    return ("For the given statement, the system response is "
+            f"'anger': {response['anger']}, 'disgust': {response['disgust']}, "
+            f"'fear': {response['fear']}, "
+            f"'joy': {response['joy']} "
+            f"and 'sadness': {response['sadness']}. "
+            f"The dominant emotion is {response['dominant_emotion']}.")
 @app.route("/")
 def render_index_page():
     """Run render_template function on the HTML"""
